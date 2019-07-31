079dea8aca01f8b9261216c5a1d54bcc505aa8df
Robert Chung <robertchung@Roberts-MacBook-Pro.local>
2019-07-31 10:45:11 -0400

    Building Beginning of Heart Rate

--- client/src/components/HeartRate.js
+++ client/src/components/HeartRate.js
@@ -1,11 +1,55 @@
 import React from 'react';
 
-const HeartRate = React.createClass({
-    render() {
-        return (
-            <div>
-                {this.props.bpm}
-            </div>
-        )
+class HeartRate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          bpm: 0,
+          count: 0,
+          msecsFirst: 0,
+          msecsPrev: 0
+        }
+        this.calcBpm = this.calcBpm.bind(this);
+      }
+  
+    calcBpm(e) {
+      var timeSeconds = new Date;
+      var msecs = timeSeconds.getTime();
+  
+      if ((msecs - this.state.msecsPrev) > 1000)
+        {
+        this.state.count = 0;
+        }
+      if (this.state.count == 0)
+        {
+        this.state.msecsFirst = msecs;
+        this.state.count = 1;
+        }
+      else
+        {
+        var bpmAvg = 60000 * this.state.count / (msecs - this.state.msecsFirst);
+        var bpmVal = Math.round(bpmAvg * 100) / 100;
+        var bpmWhole = Math.round(bpmAvg);
+        this.state.count++;
+        this.setState({
+          bpm: bpmWhole
+        });
+        }
+        this.state.msecsPrev = msecs;
+      
     }
-})
+  
+    render() {
+      return (
+        <div>
+          <h3>Heart Rate: {this.state.bpm} BPM</h3>
+          <h4>Tap the logo to the beat of your pulse</h4>
+          <div id="logo">
+          <img class="logo" src="url.png" onClick={this.calcBpm} />
+          </div>
+        </div>
+        )
+      }
+  }
+  
+  export default HeartRate;
